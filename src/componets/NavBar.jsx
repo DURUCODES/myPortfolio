712fc510ef0c34cb605bc7187cb6296029b2ab65
DURU <rockyduruchukwu@gmail.com>
2024-11-12 19:57:58 -0800

    done

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     }
   };
   return (
-    <div className="flex flex-col w-full  bg-black rounded-xl border">
+    <div className="flex flex-col w-full bg-black rounded-xl border">
       <div className="flex md:justify-between md:mb-12">
         <div>
           <h1 className="text-3xl font-bold px-4 mt-4 md:mt-0 md:pt-4 md:pb-2 mb-2 text-white">
@@ -35,7 +35,7 @@ const NavBar = () => {
           <div className="bg-yellow-400 w-16 h-2 mx-4 rounded-xl"></div>
         </div>
         <div className="bg-gradient-to-br from-[#534d5c] to-[rgba(75,30,133,0.01)] py-4 md:px-10 md:rounded-bl-2xl rounded-tl-2xl md:rounded-tl-none rounded-tr-2xl md:rounded-tr-xl border fixed bottom-0 left-0 right-0 rounded md:static backdrop-blur-[12px] z-50">
-          <ul className="flex text-white font-semibold md:px-16 px-7">
+          <ul className="flex justify-center text-white font-semibold md:px-16">
             {["About", "Resume", "Projects", "Contact"].map((item) => (
               <li
                 key={item}
